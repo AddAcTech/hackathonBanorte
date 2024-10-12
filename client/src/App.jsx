0d8d8c672e26e7aff13ed9c554110a86009cf2c0
Alexis <aaguilar@levuexecutives.com>
2024-10-12 16:47:55 -0600

    Ramses ruta

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Loading from "./components/Loading";
-import Example from "./components/Example";
 import Learning from "./components/Learning";
 import Principal from "./components/Principal";
+import Chat from "./components/Chat";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Principal />} />
           <Route path="/learning" element={<Learning />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/" element={<Chat />} />
         </Routes>
       </Router>
     </>

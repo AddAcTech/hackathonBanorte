81b112f3f1d4cb1f28d42ad639d71906b4bd011d
Alexis <aaguilar@levuexecutives.com>
2024-10-13 01:57:46 -0600

    blob?

--- server/src/controllers/document.controller.js
+++ server/src/controllers/document.controller.js
@@ -1,16 +1,16 @@
-import DocumentAI from "../api/documentAI.js"; 
+import DocumentAI from "../api/documentAI.js";
 import { generateContent } from "../api/ApiGenerative.js";
-import { console } from "inspector";
 
 export const processDocument = async (req, res) => {
-    const documentPath = req.body.path
-    const processedDocument = await DocumentAI(documentPath)
-    console.log(processedDocument)
-    const response = await generateContent(processedDocument)
+  const blobDocument = req.body;
+  const base64Document = Buffer.from(blobDocument).toString("base64");
+  console.log(base64Document);
+  //   const processedDocument = await DocumentAI(base64Document);
+  //   console.log(processedDocument);
+  //   const response = await generateContent(processedDocument);
 
-    res.status(200).json({
-        message: "Document processed successfully",
-        response:  response
-    })
+  res.status(200).json({
+    message: "Document processed successfully",
+    // response: response,
+  });
 };
-

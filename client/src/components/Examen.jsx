9ad04fd8b5d38069b55c6100bcc2e11ede186593
Alexis <aaguilar@levuexecutives.com>
2024-10-13 01:25:43 -0600

    Examen bg

--- client/src/components/Examen.jsx
+++ client/src/components/Examen.jsx
@@ -3,9 +3,9 @@ import PreguntaExamen from "./PreguntaExamen";
 
 function Examen() {
   return (
-    <div className="flex flex-col items-center w-full bg-slate-400 pt-4">
-      <h1 className="title mb-2">Bienvenido a tu examen</h1>
-      <div className="flex flex-col gap-1">
+    <div className="flex flex-col items-center w-full pt-4">
+      <h1 className="title mb-4">Bienvenido a tu examen</h1>
+      <div className="flex flex-col gap-4">
         <PreguntaExamen />
         <PreguntaExamen />
         <PreguntaExamen />
--- client/src/components/PreguntaExamen.jsx
+++ client/src/components/PreguntaExamen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PreguntaExamen() {
   return (
-    <>
+    <div>
       <h2 className="font-bold">Pregunta 1</h2>
       <p className="font-semibold">¿Cuál es la capital de México?</p>
       <div className="flex gap-2 justify-center items-center">
@@ -17,7 +17,7 @@ function PreguntaExamen() {
         <input type="radio" name="q1" value="a" />
         <label htmlFor="a">Guadalajara</label>
       </div>
-    </>
+    </div>
   );
 }
 
